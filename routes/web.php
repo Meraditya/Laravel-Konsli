91519f5f1f884447ad888e10bff1a399f0320968
raditya <meradityawahyu@gmail.com>
2025-01-29 18:14:12 +0700

    pratikum route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admins', function () {
-    return view('admins');
+Route::get('/adminss', function () {
+    return view('adminss');
 });
 
 Route::get('/home', function() {
